5e1b2fa7034f5321a3cbfec14d9ce62922b1309b
Cleberton Kobay <Clebertonkobay@gmail.com>
2023-05-25 23:09:32 -0300

    first commit

---
+++ server/src/server.ts
@@ -0,0 +1,13 @@
+import fastify from 'fastify';
+
+const app = fastify();
+
+app.get('/hello',()=>{
+    return 'hello'
+});
+
+app.listen({
+    port:3333,
+}).then(()=>{
+    console.log('✔ Sever Running on http://localhost:3333');
+})
