c222d7bca3197e257735a24668caacc9dc277ab9
Cleberton Kobay <Clebertonkobay@gmail.com>
2023-05-26 18:38:03 -0300

    Add mobile

---
+++ mobile/App.tsx
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>My second app</Text>
+      <StatusBar style="auto" translucent />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#00f',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text:{
+    color:'#f00'
+  }
+});
